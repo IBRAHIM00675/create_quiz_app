d39dc1fe7e37c617959a314c3e2501bd35eb5ec9
ibrahim musa <miharbiibra675@gmail.com>
2025-04-09 16:19:07 +0300

    added image with a text and an outlined Button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,7 +6,20 @@ class StartScreen extends StatelessWidget {
 
   @override
    Widget build(context) {
-    return Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/image/quiz-logo.png'),
+          SizedBox(height: 40,),
+          Text('Learning  Flutter the practical way!'),
+          SizedBox(height: 20,),
+
+          OutlinedButton(onPressed: (){}, child: Text('Start Quiz'))
+        ],
+
+      ),
+    );
 
    }
 }
