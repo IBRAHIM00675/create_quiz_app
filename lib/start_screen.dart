4b837ce77fadbb0d3a6a4b0f6a85061f41488bea
ibrahim musa <miharbiibra675@gmail.com>
2025-04-10 18:34:17 +0300

    style(start_screen): make text and button white

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-
- const StartScreen({super.key});
+  const StartScreen({super.key});
 
   @override
-   Widget build(context) {
+  Widget build(context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset('assets/image/quiz-logo.png'),
-          SizedBox(height: 40,),
-          Text('Learning  Flutter the practical way!'),
-          SizedBox(height: 20,),
+          SizedBox(height: 40),
+          Text(
+            'Learning  Flutter the practical way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
 
-          OutlinedButton(onPressed: (){}, child: Text('Start Quiz'))
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: BorderSide(color: Colors.white),
+            ),
+            child: Text('Start Quiz'),
+          ),
         ],
-
       ),
     );
-
-   }
+  }
 }
