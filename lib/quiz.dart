2e14bc7712fa2a0d59610e534243797f51a02cfd
ibrahim musa <miharbiibra675@gmail.com>
2025-04-11 15:16:00 +0300

    quiz add gradient background and start screen integration

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,22 +1,47 @@
+import 'package:create_quiz_app/start_screen.dart';
 import 'package:flutter/material.dart';
+
 
 class Quiz extends StatefulWidget {
 
-  
-  @override
-  _QuizState createState() => _QuizState(); // This is the correct way to return the state
+ const  Quiz({super.key});
+
+   @override
+
+ State<Quiz> createState() {
+  return _QuizeState();
+
+  }
+
 }
 
-class _QuizState extends State<Quiz> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz App'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Quiz!'),
-      ),
-    );
-  }
+
+class _QuizeState extends   State<Quiz> {
+
+
+@override
+  Widget build(context){
+
+
+    return  MaterialApp(
+    home: Scaffold(
+      body:
+       Container(
+        decoration: BoxDecoration(
+
+          gradient:LinearGradient(colors:[
+            const Color.fromARGB(255, 147, 17, 170),
+            const Color.fromARGB(255, 8, 90, 156),
+
+
+          ])
+        ),
+        
+        child:  StartScreen())
+    ),
+  );
+
+
+
+   }
 }
