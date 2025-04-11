d7336f71e340bd98242605588036145d80483ab9
ibrahim musa <miharbiibra675@gmail.com>
2025-04-11 15:39:04 +0300

    transported material app to quiz file and imported the Quiz class

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
-import 'package:create_quiz_app/start_screen.dart';
+import 'package:create_quiz_app/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body:
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              const Color.fromARGB(255, 76, 12, 239),
-              const Color.fromARGB(255, 179, 32, 205),
-            ])
-          ),
-          
-          child: StartScreen(),
-          ),
-      ),
-    )
+     Quiz(),
   );
 }
